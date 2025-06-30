b3c44e5724ad5efc56d5a8e4051d896da120e9bb
DenisVinokurov <denisv@vedisoft.ru>
2025-07-01 00:46:29 +0500

    master: added Document Entity

---
+++ src/Entity/Document.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DocumentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DocumentRepository::class)]
+class Document
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::BIGINT)]
+    private ?string $product_id = null;
+
+    #[ORM\Column]
+    private ?\DateTime $report_date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $value = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $balance = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2, nullable: true)]
+    private ?string $price = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $inventory_error = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductId(): ?string
+    {
+        return $this->product_id;
+    }
+
+    public function setProductId(string $product_id): static
+    {
+        $this->product_id = $product_id;
+
+        return $this;
+    }
+
+    public function getReportDate(): ?\DateTime
+    {
+        return $this->report_date;
+    }
+
+    public function setReportDate(\DateTime $report_date): static
+    {
+        $this->report_date = $report_date;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(?int $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getBalance(): ?int
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(?int $balance): static
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?string $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getInventoryError(): ?int
+    {
+        return $this->inventory_error;
+    }
+
+    public function setInventoryError(?int $inventory_error): static
+    {
+        $this->inventory_error = $inventory_error;
+
+        return $this;
+    }
+}
